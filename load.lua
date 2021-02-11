f0c177693e9303d4ccf7a38ab1fc57125e119eae
jcgriffin1 <jdoggriffin123@gmail.com>
2021-02-11 00:45:26 -0500

    Push for basic set up for functio prototyping

--- load.lua
+++ load.lua
@@ -1,6 +1,10 @@
 --Developers: jcgiffin and 1 other
 --Build Date: Wed Feb 10 2021
 --Purpose:Load additional lua files for ai modules and asio code
-
+require "ai.ai_loader"
 io.write("[LUA]:Hello World\n")
+
+function Preloadfile(target)
+    io.write("Set up", target)
+end
 
